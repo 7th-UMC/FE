6e353c759d93df3fae2e3652774de983c178c738
sheepyis <zx9086@naver.com>
2024-08-14 15:37:04 +0900

    ✨ photo_사진 pdf 저장

--- src/pages/Photo/PhotoResult.jsx
+++ src/pages/Photo/PhotoResult.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import colors from '../../styles/colors';
 import FrameData from '../../utils/Photo/frameData';
 import { useLocation } from 'react-router-dom';
+import html2canvas from 'html2canvas';
 
 const ResultContainer = styled.div`
     width: 60%;
@@ -20,16 +21,16 @@ const ImgContainer = styled.div`
     justify-content: center;
     align-items: flex-start;
     overflow: hidden;
+    transition: transform 0.3s;
 
     @media screen and (max-width: 430px) {
-        rotate: 90deg;
+        margin-top: 4rem;
     }
 `;
 
 const FrameContainer = styled.img`
     width: 100%;
     height: 80%;
-    z-index: 999;
 `;
 
 const PhotoGallery = styled.div`
@@ -40,6 +41,7 @@ const PhotoGallery = styled.div`
     justify-content: center;
     align-items: flex-end;
     position: absolute;
+    z-index: -1;
     margin-top: -2%;
 `;
 
@@ -55,10 +57,9 @@ const FrameButtonContainer = styled.div`
     align-items: center;
     gap: 1.2rem;
     margin-top: ${({ isselectedframe3 }) => (isselectedframe3 === "true" ? '6.5rem' : '4.841rem')};
-    z-index: 9999;
 
     @media screen and (max-width: 430px) {
-        margin-top: ${({ isselectedframe3 }) => (isselectedframe3 === "true" ? '12.5rem' : '12rem')};
+        margin-top: ${({ isselectedframe3 }) => (isselectedframe3 === "true" ? '3rem' : '2.42rem')};
         gap: 0.6rem;
     }
 `;
@@ -100,24 +101,38 @@ const SubmitButton = styled.div`
         border-radius: 0.4rem;
         font-size: 1.6rem;
         line-height: 1.9rem;
-        margin-top: 5rem;;
+        margin-top: 5rem;
     }
-`
+`;
 
 const PhotoResult = () => {
     const location = useLocation();
     const state = location.state || { photos: [] };
     const [selectedFrame, setSelectedFrame] = useState(FrameData[0].frameWeb);
-        const isSelectedFrame3 = FrameData.find(frame => frame.frameWeb === selectedFrame)?.id === 3;
+    const [rotate, setRotate] = useState(false);
+    const imgContainerRef = useRef(null);
+    const isSelectedFrame3 = FrameData.find(frame => frame.frameWeb === selectedFrame)?.id === 3;
 
     const handleFrameChange = (frame) => {
         setSelectedFrame(frame);
     };
 
+    const handleSubmit = () => {
+        if (imgContainerRef.current) {
+
+            html2canvas(imgContainerRef.current).then(canvas => {
+                const link = document.createElement('a');
+                link.href = canvas.toDataURL('image/png');
+                link.download = 'photo-result.png';
+                link.click();
+            });
+        }
+    };
+
     return (
         <div className="pageContainer" style={{ display: 'flex', flexDirection: "column", alignItems: 'center' }}>
             <ResultContainer>
-                <ImgContainer>
+                <ImgContainer ref={imgContainerRef} rotate={rotate}>
                     <FrameContainer src={selectedFrame} alt="Frame" />
                     <PhotoGallery>
                         {state.photos.map((photo, index) => (
@@ -133,7 +148,7 @@ const PhotoResult = () => {
                 ))}
             </FrameButtonContainer>
 
-            <SubmitButton>전송하기</SubmitButton>
+            <SubmitButton onClick={handleSubmit}>전송하기</SubmitButton>
         </div>
     );
 };
