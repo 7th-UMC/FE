b5ab10329b6e0ead068344ac6adc32a1d67dbb99
hyesngy <tell280210@gmail.com>
2024-08-30 22:36:03 +0900

    ✨ home_intro 애니메이션 적용

--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import colors from "../../../styles/colors";
 import IntroLogo1 from "../../../assets/images/Home/Intro/introLogo1.png";
 import IntroLogo2 from "../../../assets/images/Home/Intro/introLogo2.png";
+import useScrollAnimation from "../../../hooks/Home/useScrollAnimation";
+import { fadeInUp, bounceIn, bounceOut } from "../../../styles/animations";
 
 const IntroContainer = styled.div`
     width: 100%;
@@ -13,6 +15,37 @@ const IntroContainer = styled.div`
     @media screen and (max-width: 430px) {
         margin: 3.282rem 0 4.4rem 0;
     }
+
+    &.visible > img {
+        animation: ${bounceIn} 1s ease-out forwards;
+    }
+
+    &.hidden > img {
+        animation: ${bounceOut} 1s ease-out forwards;
+    }
+
+    &.visible > div > p:nth-child(1) {
+        opacity: 0;
+        animation: ${fadeInUp} 1s ease-out forwards;
+        animation-delay: 0s;
+    }
+
+    &.visible > div > p:nth-child(2) {
+        opacity: 0;
+        animation: ${fadeInUp} 1s ease-out forwards;
+        animation-delay: 0.3s;
+    }
+
+    &.visible > div > p:nth-child(3) {
+        opacity: 0;
+        animation: ${fadeInUp} 1s ease-out forwards;
+        animation-delay: 0.6s;
+    }
+
+    &.hidden > div > p {
+        opacity: 0;
+        transform: translateY(20px);
+    }
 `
 
 const IntroLogoImg = styled.img`
@@ -22,6 +55,13 @@ const IntroLogoImg = styled.img`
         width: 4.8rem;
         content: url(${IntroLogo2});
     }
+`
+
+const IntroTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
 `
 
 const IntroP = styled.p`
@@ -75,10 +115,12 @@ const IntroP3 = styled.p`
 `
 
 const Intro = () => {
-    return (
-        <IntroContainer>
-            <IntroLogoImg src={IntroLogo1} alt="intro" />
+    const { elementRef } = useScrollAnimation(); 
 
+    return (
+        <IntroContainer ref={elementRef}>
+          <IntroLogoImg src={IntroLogo1} alt="intro" />
+          <IntroTextContainer>
             <IntroP>UMC란?</IntroP>
             <IntroP2>앱&웹 서비스 런칭에 도전하는 대학생 IT 연합동아리</IntroP2>
             <IntroP3>
@@ -86,6 +128,7 @@ const Intro = () => {
                 <span>는 2021년 8월을 시작으로, IT 프로젝트 경험과 네트워킹 기회를 제공하기 위해 결성되었습니다. 서비스를 직접 기획하고 개발할 수 있는 기회를 얻을 수 있으며, IT 역량 향상과 UX/UI 실무 경험을 축적할 수 있습니다.
                 </span>
             </IntroP3>
+          </IntroTextContainer>
         </IntroContainer>
     )
 }
