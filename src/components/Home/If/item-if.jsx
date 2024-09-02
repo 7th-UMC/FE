a7e80153dbd2b13a35c23ef2cc5ce69727c6f306
hyesngy <tell280210@gmail.com>
2024-09-02 19:00:43 +0900

    💫 home_if 섹션 애니메이션 옵션 추가

--- src/components/Home/If/item-if.jsx
+++ src/components/Home/If/item-if.jsx
@@ -76,7 +76,7 @@ const ItemIf = ({ id, homeWeb, homeMobile, title, explain }) => {
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
-      transition={{ duration: 0.6 }}
+      transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
       viewport={{ once: false, amount: 0.6 }}
     >
       <IfInnerImg src={isMobile ? homeMobile : homeWeb} alt={title} />
